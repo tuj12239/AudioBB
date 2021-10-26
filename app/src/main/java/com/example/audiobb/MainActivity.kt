d064407b4719bc38cc4b5719ad26a634d84dbd5c
Isaac <tuj12239@temple.edu>
2021-10-26 14:27:38 -0400

    BookList is initialized with string array resources.

--- app/src/main/java/com/example/audiobb/MainActivity.kt
+++ app/src/main/java/com/example/audiobb/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = initBooks()
+
+    }
+
+    private fun initBooks(): BookList {
+        val list = BookList()
+        val names = resources.getStringArray(R.array.book_names)
+        val authors = resources.getStringArray(R.array.book_authors)
+
+        for (i in names.indices) {
+            list.add(Book(names[i], authors[i]))
+        }
+
+        return list
     }
 }
