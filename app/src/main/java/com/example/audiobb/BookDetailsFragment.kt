d7fbdad61fc8c3c2f2f7d4389c5d5e19d37de8cc
Isaac <tuj12239@temple.edu>
2021-10-26 16:33:40 -0400

    Second fragment shows desired data and allows return to first fragment.

--- app/src/main/java/com/example/audiobb/BookDetailsFragment.kt
+++ app/src/main/java/com/example/audiobb/BookDetailsFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class BookDetailsFragment : Fragment() {
+
+    lateinit var layout: View
+    lateinit var name: TextView
+    lateinit var author: TextView
 
     companion object {
         @JvmStatic
@@ -20,10 +26,32 @@ class BookDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false)
+        layout = inflater.inflate(R.layout.fragment_book_details, container, false)
+        return layout
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        name = layout.findViewById(R.id.bookTitleLabel)
+        author = layout.findViewById(R.id.bookAuthorLabel)
+
+        ViewModelProvider(requireActivity())
+            .get(BookViewModel::class.java)
+            .getSelectedBook()
+            .observe(viewLifecycleOwner, {updateLabels()})
+
+    }
+
+    private fun updateLabels() {
+        val book = ViewModelProvider(requireActivity())
+            .get(BookViewModel::class.java)
+            .getSelectedBook()
+
+        name.text = book.value?.name
+        author.text = book.value?.author
+    }
 
 }
