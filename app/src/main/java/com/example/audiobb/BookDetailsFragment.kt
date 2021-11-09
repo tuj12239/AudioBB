620126e747ccb4597eb773b9a69c40218f9d408e
Isaac <tuj12239@temple.edu>
2021-11-09 14:57:36 -0500

    Image is now displayed with the Picasso library

--- app/src/main/java/com/example/audiobb/BookDetailsFragment.kt
+++ app/src/main/java/com/example/audiobb/BookDetailsFragment.kt
@@ -1,18 +1,23 @@
 package com.example.audiobb
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
+import java.net.URL
 
 class BookDetailsFragment : Fragment() {
 
     lateinit var layout: View
     lateinit var name: TextView
     lateinit var author: TextView
+    lateinit var cover: ImageView
 
     companion object {
         @JvmStatic
@@ -37,6 +42,7 @@ class BookDetailsFragment : Fragment() {
 
         name = layout.findViewById(R.id.bookTitleLabel)
         author = layout.findViewById(R.id.bookAuthorLabel)
+        cover = layout.findViewById(R.id.bookCoverView)
 
         ViewModelProvider(requireActivity())
             .get(BookViewModel::class.java)
@@ -52,6 +58,7 @@ class BookDetailsFragment : Fragment() {
 
         name.text = book.value?.name
         author.text = book.value?.author
+        Picasso.get().load(book.value?.coverURL).into(cover)
     }
 
 }
